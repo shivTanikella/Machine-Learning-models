86c223368a8ab49ba511e6897eb5c03e21ebdf81
shivTanikella <tanikellashiv@gmail.com>
2025-05-27 16:29:19 -0400

    Added a function that determines the threshold for n_iter

--- linear_regression_scratch.py
+++ linear_regression_scratch.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 
 
 class sLinearRegression:
-    def __init__(self, learning_rate=0.01, bias=0, n_iters=1000):
+    def __init__(self, learning_rate=0.01, bias=0, n_iters=100):
         # h(x) = bias + weights_1*x_1 + ... + weight_n*x_n
         self.lr=learning_rate
         self.weights=None
@@ -32,6 +32,22 @@ class sLinearRegression:
             self.weights-= self.lr*dweights
             self.bias-=self.lr*dbias
 
+    def iter_threshold(self, X_train, X_test, y_train, y_test, max_iter=100):
+        lr=sLinearRegression(n_iters=max_iter)
+        lr.fit(X_train, y_train)
+        predict_base = lr.predict(X_test)
+        mse_base = mean_squared_error(y_test, predict_base)
+        diff=20
+        while (diff>10):
+            max_iter+=100
+            lr.fit(X_train, y_train)
+            predict=lr.predict(X_test)
+            mse_new=mean_squared_error(y_test, predict)
+            diff=abs(mse_new-mse_base)
+            mse_base=mse_new
+        print("max_iter:", max_iter)
+        self.n_iters=max_iter
+
     def predict(self,X):
         return self.y_prediction(X)
     
@@ -48,6 +64,7 @@ sX_train = standardized.transform(X_train)
 sX_test = standardized.transform(X_test)
 
 slr = sLinearRegression()
+slr.iter_threshold(sX_train, sX_test, y_train, y_test)
 slr.fit(sX_train,y_train)
 spredict = slr.predict(sX_test)
 s_mse = mean_squared_error(y_test, spredict)
@@ -59,6 +76,7 @@ print(f"FROM-SCRATCH MODEL:\nCaliforniaHousing dataset\nRMSE: {s_rmse}\nr2_score
 X2,y2=load_diabetes(return_X_y=True)
 slr2 = sLinearRegression()
 X2_train, X2_test, y2_train, y2_test = train_test_split(X2,y2,test_size=0.2,random_state=1)
+slr2.iter_threshold(X2_train, X2_test, y2_train, y2_test)
 slr2.fit(X2_train,y2_train)
 spredict = slr2.predict(X2_test)
 s_mse = mean_squared_error(y2_test,spredict)
@@ -70,6 +88,7 @@ print(f"\nDiabetes dataset\nRMSE: {s_rmse}\nr2_score: {s_r2}")
 X3,y3 = make_regression(n_samples=100, n_features=2,noise=10,random_state=42)
 slr3 = sLinearRegression()
 X3_train, X3_test, y3_train, y3_test = train_test_split(X3,y3,test_size=0.2,random_state=1)
+slr2.iter_threshold(X3_train, X3_test, y3_train, y3_test)
 slr3.fit(X3_train,y3_train)
 spredict = slr3.predict(X3_test)
 s_mse=mean_squared_error(y3_test,spredict)
@@ -98,6 +117,9 @@ print(f"\nDiabetes dataset\nRMSE: {rmse}\nr2_score: {r2}")
 lr3 = LinearRegression().fit(X3_train,y3_train)
 predict = lr3.predict(X3_test)
 mse = mean_squared_error(y3_test, predict)
+
+print("MSE:", mse)
+
 rmse=mse**0.5
 r2=r2_score(y3_test,predict)
 print(f"\nmake_regression dataset\nRMSE: {rmse}\nr2_score: {r2}")
